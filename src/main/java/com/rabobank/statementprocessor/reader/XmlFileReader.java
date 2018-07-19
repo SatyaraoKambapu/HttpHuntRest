3d52920cdfd80da8c4f8b2d51cfb143e8189bccd
satyarao kambapu <skambapu@altimetrik.com>
2018-07-19 18:19:56 +0530

    SRP principle used properly.

--- src/main/java/com/rabobank/statementprocessor/reader/XmlFileReader.java
+++ src/main/java/com/rabobank/statementprocessor/reader/XmlFileReader.java
@@ -1,29 +1,18 @@
 package com.rabobank.statementprocessor.reader;
 
-import org.springframework.batch.item.ItemReader;
 import org.springframework.batch.item.xml.StaxEventItemReader;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
 
-import com.rabobank.statementprocessor.entity.CustomerRecord;
+import com.rabobank.statementprocessor.entity.CustomerRecords;
 
-public class XmlFileReader {
+public class XmlFileReader extends StaxEventItemReader<CustomerRecords> {
 
-	@Value("${filepath}")
-	private String filepath;
-
-	@Bean
-	ItemReader<CustomerRecord> xmlFileItemReader() {
-		StaxEventItemReader<CustomerRecord> xmlFileReader = new StaxEventItemReader<>();
-		xmlFileReader.setResource(new ClassPathResource(filepath));
-		xmlFileReader.setFragmentRootElementName("record");
-
-		Jaxb2Marshaller studentMarshaller = new Jaxb2Marshaller();
-		studentMarshaller.setClassesToBeBound(CustomerRecord.class);
-		xmlFileReader.setUnmarshaller(studentMarshaller);
-
-		return xmlFileReader;
+	public XmlFileReader(String filepath) {
+		this.setResource(new FileSystemResource(filepath));
+		this.setFragmentRootElementName("records");
+		Jaxb2Marshaller recordsMarshaller = new Jaxb2Marshaller();
+		recordsMarshaller.setClassesToBeBound(CustomerRecords.class);
+		this.setUnmarshaller(recordsMarshaller);
 	}
 }
